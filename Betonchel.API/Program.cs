7673610669eb9ab4ee4b149e2b28e6899110ac81
GlebMoskalev <gleb12122003@gmail.com>
2024-03-31 13:52:20 +0500

    Add API

--- Betonchel.API/Program.cs
+++ Betonchel.API/Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
+//
+// app.MapGet("/", () => "Hello World!");
+//
+// app.Run();
 
-app.MapGet("/", () => "Hello World!");
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-app.Run();
+namespace Betonchel.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
+
